8526e6dbd02d1fc4e875f8ccb29e151f05221ef6
Tim Hütz <tim@huetz.biz>
2019-05-27 17:35:23 +0200

    :heavy_plus_sign: add the futures dependency

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,7 @@
-use log::{debug, info};
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use log::{debug, error, info};
 use serde_json::json;
 use serde_json::Value;
 use simplelog::{CombinedLogger, Config, LevelFilter, TermLogger, WriteLogger};
@@ -21,6 +24,33 @@ fn get_version_str() -> String {
         env!("CARGO_PKG_VERSION_PATCH"),
         option_env!("CARGO_PKG_VERSION_PRE").unwrap_or("")
     )
+}
+
+fn store_measurement(sensor: &str, body: &Body) -> Response<Body> {
+    // be sure that the sensor is allowed to post to this endpoint
+    if !VALID_SENSORS.contains(&sensor) {
+        return Response::builder()
+            .status(StatusCode::FORBIDDEN)
+            .body(Body::from(""))
+            .unwrap();
+    }
+
+    // everything was successfully done, we can return a successful state
+    return Response::builder()
+        .status(StatusCode::NO_CONTENT)
+        .body(Body::from(""))
+        .unwrap();
+}
+
+fn service_handler(request: Request<Body>) -> Response<Body> {
+    if request.uri().path().starts_with("/v1/measurement") && request.method() == Method::POST {
+        let sensor_id = request.uri().path().rsplit('/').next().unwrap_or("");
+        return store_measurement(sensor_id, request.body());
+    }
+    return Response::builder()
+        .status(StatusCode::BAD_REQUEST)
+        .body(Body::from(""))
+        .unwrap();
 }
 
 fn main() {
@@ -52,4 +82,12 @@ fn main() {
     for sensor_id in VALID_SENSORS.iter() {
         info!("{} is a valid sensor identifier", sensor_id);
     }
+
+    // configure the server and start it up
+    let server_address = ([0, 0, 0, 0], 8000).into();
+    let new_service = || service_fn_ok(service_handler);
+    let server = Server::bind(&server_address)
+        .serve(new_service)
+        .map_err(|e| error!("server error: {}", e));
+    hyper::rt::run(server);
 }
