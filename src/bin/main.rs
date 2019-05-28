b0479b06c14fedb55967faffe5f9e3a3a5f8fb7f
Tim Hütz <tim@huetz.biz>
2019-05-28 12:23:21 +0200

    :construction: finish converting to a hyper-only solution

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,12 +1,15 @@
+use futures::future::FlattenStream;
 use futures::stream::Stream;
 use hyper::rt::Future;
 use hyper::service::service_fn_ok;
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
-use log::{debug, error, info};
+use log::{debug, error, info, warn};
 use serde_json::json;
 use serde_json::Value;
 use simplelog::{CombinedLogger, Config, LevelFilter, TermLogger, WriteLogger};
+use std::convert::TryInto;
 use std::fs::File;
+use std::str;
 use weather_station_backend::boundary::Measurement;
 
 // a (currently) hard coded list of all valid sensor IDs
@@ -28,46 +31,62 @@ fn get_version_str() -> String {
     )
 }
 
-fn store_measurement(sensor: &str, body: &Body) -> Response<Body> {
-    // be sure that the sensor is allowed to post to this endpoint
-    if !VALID_SENSORS.contains(&sensor) {
-        return Response::builder()
-            .status(StatusCode::FORBIDDEN)
-            .body(Body::from(""))
-            .unwrap();
-    }
-
-    // wait until we received all the content of the body
-    let body_content = body.concat2().map(|v| v);
-
-    // convert the received measurement to the corresponding object
-    let measurement: Measurement = serde_json::from_str("").unwrap_or(Measurement {
-        temperature: 0.0,
-        humidity: 0.0,
-        pressure: 0.0,
-    });
-
-    // temporary log the received values
-    error!(
-        "Temp: {} :: Humidity: {} :: Pressure: {}",
-        measurement.temperature, measurement.humidity, measurement.pressure
-    );
-
-    // everything was successfully done, we can return a successful state
-    return Response::builder()
-        .status(StatusCode::NO_CONTENT)
-        .body(Body::from(""))
-        .unwrap();
-}
-
 fn service_handler(request: Request<Body>) -> Response<Body> {
-    if request.uri().path().starts_with("/v1/measurement") && request.method() == Method::POST {
-        let sensor_id = request.uri().path().rsplit('/').next().unwrap_or("");
-        return store_measurement(sensor_id, request.body());
+    let (parts, body) = request.into_parts();
+    if parts.uri.path().starts_with("/v1/measurement") && parts.method == Method::POST {
+        let sensor_id = parts.uri.path().rsplit('/').next().unwrap_or("");
+
+        // be sure that the sensor is allowed to post to this endpoint
+        if !VALID_SENSORS.contains(&sensor_id) {
+            warn!(
+                "A unknown sensor with the ID {} tried to push its data to this server instance.",
+                sensor_id
+            );
+            return Response::builder()
+                .status(StatusCode::FORBIDDEN)
+                .body(Body::empty())
+                .unwrap();
+        }
+
+        // wait until we received all the content of the body
+        let entire_body = body.concat2();
+        let response = entire_body.map(|body| {
+            match str::from_utf8(body.as_ref()) {
+                Ok(v) => {
+                    // convert the received measurement to the corresponding object
+                    let measurement: Measurement = serde_json::from_str(v).unwrap_or(Measurement {
+                        temperature: 0.0,
+                        humidity: 0.0,
+                        pressure: 0.0,
+                    });
+
+                    // temporary log the received values
+                    error!(
+                        "Temp: {} :: Humidity: {} :: Pressure: {}",
+                        measurement.temperature, measurement.humidity, measurement.pressure
+                    );
+
+                    // everything was successfully done, we can return a successful state
+                    return Response::builder()
+                        .status(StatusCode::NO_CONTENT)
+                        .body(Body::empty())
+                        .unwrap();
+                }
+                Err(_e) => {
+                    error!("Could not convert received request body to a string!");
+                    return Response::builder()
+                        .status(StatusCode::BAD_REQUEST)
+                        .body(Body::empty())
+                        .unwrap();
+                }
+            }
+        });
+
+        return response.wait().unwrap();
     }
     return Response::builder()
         .status(StatusCode::BAD_REQUEST)
-        .body(Body::from(""))
+        .body(Body::empty())
         .unwrap();
 }
 
