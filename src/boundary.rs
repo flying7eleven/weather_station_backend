4165ca0a9d6938758af276bcf6a0be212f00b2f3
Tim Hütz <tim@huetz.biz>
2019-05-29 13:25:36 +0200

    :recycle: ensure that the sensor id is in the measurement payload

--- src/boundary.rs
+++ src/boundary.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct Measurement {
+    pub sensor: String,
     pub temperature: f32,
     pub humidity: f32,
     pub pressure: f32,
