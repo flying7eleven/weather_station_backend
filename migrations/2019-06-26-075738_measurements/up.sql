f54797e816a88492a88616cdd9a41ee048d8ac4e
Tim Hütz <tim@huetz.biz>
2019-06-30 09:44:51 +0200

    :sparkles: change the table structure and the db creds to mysql

--- migrations/2019-06-26-075738_measurements/up.sql
+++ migrations/2019-06-26-075738_measurements/up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE measurements
 (
-    id          INTEGER PRIMARY KEY NOT NULL,
-    time        DATETIME            NOT NULL,
-    sensor      TEXT                NOT NULL,
-    temperature FLOAT               NOT NULL,
-    humidity    FLOAT               NOT NULL,
-    pressure    FLOAT               NOT NULL
+    id          INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    time        DATETIME                           NOT NULL,
+    sensor      TEXT                               NOT NULL,
+    temperature FLOAT                              NOT NULL,
+    humidity    FLOAT                              NOT NULL,
+    pressure    FLOAT                              NOT NULL
 )
