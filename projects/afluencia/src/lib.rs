43262e45d4a71d485f890772eb992d18f7b112a5
Tim Hütz <tim@huetz.biz>
2019-07-09 11:11:25 +0200

    :sparkles: do actually send the measurement to InfluxDB

--- projects/afluencia/src/lib.rs
+++ projects/afluencia/src/lib.rs
@@ -2,15 +2,14 @@ use dotenv::dotenv;
 use futures::{self, Future, Stream};
 use hyper::{header::HeaderValue, header::CONTENT_TYPE, rt, Body, Client, Method, Request, Uri};
 use log::{debug, error};
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::env;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// this part is used from https://github.com/driftluo/InfluxDBClient-rs/blob/master/src/keys.rs by
-// the github user driftluo
+// this part is used from https://github.com/driftluo/InfluxDBClient-rs by the github user driftluo
 
 /// Influxdb value, Please look at [this address](https://docs.influxdata.com/influxdb/v1.3/write_protocols/line_protocol_reference/)
 pub enum Value {
@@ -22,8 +21,8 @@ pub enum Value {
 
 pub struct DataPoint {
     pub measurement: String,
-    pub tags: HashMap<String, Value>,
-    pub fields: HashMap<String, Value>,
+    pub tags: BTreeMap<String, Value>,
+    pub fields: BTreeMap<String, Value>,
     pub timestamp: Option<i64>,
 }
 
@@ -31,8 +30,8 @@ impl DataPoint {
     pub fn new(measurement: &str) -> DataPoint {
         DataPoint {
             measurement: String::from(measurement),
-            tags: HashMap::new(),
-            fields: HashMap::new(),
+            tags: BTreeMap::new(),
+            fields: BTreeMap::new(),
             timestamp: None,
         }
     }
@@ -51,6 +50,88 @@ impl DataPoint {
         self.timestamp = Some(timestamp);
         self
     }
+}
+
+#[inline]
+fn escape_measurement(value: &str) -> String {
+    value.replace(",", "\\,").replace(" ", "\\ ")
+}
+
+#[inline]
+fn escape_keys_and_tags(value: &str) -> String {
+    value
+        .replace(",", "\\,")
+        .replace("=", "\\=")
+        .replace(" ", "\\ ")
+}
+
+#[inline]
+fn escape_string_field_value(value: &str) -> String {
+    format!("\"{}\"", value.replace("\"", "\\\""))
+}
+
+pub fn line_serialization(point: DataPoint) -> String {
+    let mut line = Vec::new();
+    line.push(escape_measurement(&point.measurement));
+
+    for (tag, value) in point.tags {
+        line.push(",".to_string());
+        line.push(escape_keys_and_tags(&tag));
+        line.push("=".to_string());
+
+        match value {
+            Value::String(s) => line.push(escape_keys_and_tags(&s)),
+            Value::Float(f) => line.push(f.to_string()),
+            Value::Integer(i) => line.push(i.to_string() + "i"),
+            Value::Boolean(b) => line.push({
+                if b {
+                    "true".to_string()
+                } else {
+                    "false".to_string()
+                }
+            }),
+        }
+    }
+
+    let mut was_first = true;
+
+    for (field, value) in point.fields {
+        line.push(
+            {
+                if was_first {
+                    was_first = false;
+                    " "
+                } else {
+                    ","
+                }
+            }
+            .to_string(),
+        );
+        line.push(escape_keys_and_tags(&field));
+        line.push("=".to_string());
+
+        match value {
+            Value::String(s) => line.push(escape_string_field_value(&s.replace("\\\"", "\\\\\""))),
+            Value::Float(f) => line.push(f.to_string()),
+            Value::Integer(i) => line.push(i.to_string() + "i"),
+            Value::Boolean(b) => line.push({
+                if b {
+                    "true".to_string()
+                } else {
+                    "false".to_string()
+                }
+            }),
+        }
+    }
+
+    if let Some(t) = point.timestamp {
+        line.push(" ".to_string());
+        line.push(t.to_string());
+    }
+
+    line.push("\n".to_string());
+
+    line.join("")
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -91,12 +172,13 @@ impl AfluenciaClient {
         self
     }
 
-    pub fn write_measurement(&self, _measurement: DataPoint) {
+    pub fn write_measurement(&self, measurement: DataPoint) {
         let target_url: Uri = self.get_write_base_url().parse().unwrap();
         let client = Client::new();
 
         // prepare the actual request to the influx server
-        let mut data_request = Request::new(Body::from("afluencia,mytag=1 myfield=90 1463683075"));
+        let measurement_line = line_serialization(measurement);
+        let mut data_request = Request::new(Body::from(measurement_line));
         *data_request.method_mut() = Method::POST;
         *data_request.uri_mut() = target_url.clone();
         data_request.headers_mut().insert(
@@ -194,5 +276,20 @@ mod tests {
             "http://hostname:1234/write?db=test&u=username&p=password",
             client.get_write_base_url()
         );
+    }
+
+    #[test]
+    fn line_serialization_of_valid_datapoint_works() {
+        let mut test_data_point = DataPoint::new("measurement_name");
+        test_data_point.add_tag("testtag", Value::String(String::from("tagvalue")));
+        test_data_point.add_field("string_field", Value::String(String::from("string_value")));
+        test_data_point.add_field("float_field", Value::Float(1.2345));
+        test_data_point.add_field("int_field", Value::Integer(12345));
+        test_data_point.add_field("bool_field", Value::Boolean(true));
+        test_data_point.add_timestamp(1_234_567_890);
+
+        let serialized_data_point = line_serialization(test_data_point);
+
+        assert_eq!("measurement_name,testtag=tagvalue bool_field=true,float_field=1.2345,int_field=12345i,string_field=\"string_value\" 1234567890\n", serialized_data_point);
     }
 }
