99b07be06647c9fcb08568fba5b6d73d59bd1426
Tim Hütz <tim@huetz.biz>
2019-09-01 12:29:30 +0200

    :white_check_mark: update the post script for manual testing

--- post_measurement.sh
+++ post_measurement.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -v -d '{"sensor":"DEADBEEF", "temperature":32.1, "humidity": 56.1, "pressure": 1003.5}' -H "Content-Type: application/json" -X POST http://localhost:8000/v1/sensor/measurement
+curl -v -d '{"temperature":27.05,"humidity":37.95,"pressure":1011.72,"raw_voltage":811.00,"sensor":"DEADBEEF"}' -H "Content-Type: application/json" -X POST http://localhost:8000/v1/sensor/measurement
