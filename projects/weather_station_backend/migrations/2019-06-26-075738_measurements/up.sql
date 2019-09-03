1968c64a4b7482ffdfc5a9581a515a52dfb4f06e
Tim Hütz <tim@huetz.biz>
2019-09-03 07:21:36 +0200

    :fire: remove the old migration scripts

--- projects/weather_station_backend/migrations/2019-06-26-075738_measurements/up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE measurements
-(
-    id          INTEGER PRIMARY KEY NOT NULL,
-    time        DATETIME            NOT NULL,
-    sensor      TEXT                NOT NULL,
-    temperature FLOAT               NOT NULL,
-    humidity    FLOAT               NOT NULL,
-    pressure    FLOAT               NOT NULL
-)
