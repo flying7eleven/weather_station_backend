dcc8a6a045a9d9bdbee023d350855d70ccfb3ef5
Tim Hütz <tim@huetz.biz>
2019-09-05 13:31:25 +0200

    :sparkles: update the post script to use all possible parameter

--- post_measurement.sh
+++ post_measurement.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -v -d '{"temperature":27.05,"humidity":37.95,"pressure":1011.72,"raw_voltage":713.00,"charge":51.13,"sensor":"DEADBEEF"}' -H "Content-Type: application/json" -X POST http://localhost:8000/v1/sensor/measurement
+curl -v -d '{"temperature":27.05,"humidity":37.95,"pressure":1011.72,"raw_voltage":713.00,"charge":51.13,"sensor":"DEADBEEF","version":"0.0.1-dev"}' -H "Content-Type: application/json" -X POST http://localhost:8000/v1/sensor/measurement
